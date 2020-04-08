c6f7f3a28825f8c6ed283dc959a6e268f038c9ad
jacobmilhon <58615265+jacobmilhon@users.noreply.github.com>
2020-04-08 14:45:32 -0500

    Math assignment

---
+++ week1/math/app.js
@@ -0,0 +1,13 @@
+const { add, subtract, multiply, divide } = require("./math.js");
+
+//additin example:
+console.log(add(3, 4));
+
+//subtraction example:
+console.log(subtract(7, 5));
+
+//multiplication example:
+console.log(multiply(10, 10));
+
+//division example:
+console.log(divide(12, 3));
---
+++ week1/math/math.js
@@ -0,0 +1,19 @@
+//4 math functions for addition, subtraction, multiplication, and divison
+
+function add(a, b) {
+    return a + b;
+};
+
+function subtract(a, b) {
+    return a - b;
+};
+
+function multiply(a, b) {
+    return a * b;
+};
+
+function divide(a, b) {
+    return a / b;
+};
+
+module.exports = { add, subtract, multiply, divide};
